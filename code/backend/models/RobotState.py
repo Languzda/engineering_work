86ab95943b68e7c450ab88630dcdf4702cdc3b81
Languzda <langusda@gmail.com>
2023-12-11 18:04:16 +0100

    fix bug with imports

--- code/backend/models/RobotState.py
+++ code/backend/models/RobotState.py
@@ -1,9 +1,9 @@
 from pydantic import BaseModel,validator, field_validator, model_validator
-from Container import Container
-from Sensor import Sensor
-import json
-import asyncio
-import connection
+from .Container import Container
+from .Sensor import Sensor
+# import json
+# import asyncio
+# import connection
 
 class RobotState(BaseModel):
     containers: list[Container]
@@ -17,15 +17,15 @@ class RobotState(BaseModel):
     robort_working: bool
     photo_url: str
 
-    async def send_state_to_clients(self,ws_connections: set):
-        if ws_connections:
-            update_message = {"event": "update", "data": self.model_dump()}
-            update_message_str = json.dumps(update_message)
-            send=asyncio.create_task(*(client.send_text(update_message_str) for client in ws_connections))
-            await send
+    # async def send_state_to_clients(self,ws_connections: set):
+    #     if ws_connections:
+    #         update_message = {"event": "update", "data": self.model_dump()}
+    #         update_message_str = json.dumps(update_message)
+    #         send=asyncio.create_task(*(client.send_text(update_message_str) for client in ws_connections))
+    #         await send
 
-    @model_validator(mode='after')
-    async def on_update(self):
-       await self.send_state_to_clients(connection.connected_clients.con_ws)
+    # @model_validator(mode='after')
+    # async def on_update(self):
+    #    await self.send_state_to_clients(connection.connected_clients.con_ws)
    
 
